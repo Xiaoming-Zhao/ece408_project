cb88cf23ad60d2f904df1454394def629bf8cd16
Xiaoming Zhao <Xiaoming.Zhao9@gmail.com>
2018-04-18 02:08:33 -0500

    milestone3

--- ece408_src/new-forward-sharemem.cuh
+++ ece408_src/new-forward-sharemem.cuh
@@ -41,32 +41,64 @@ __global__ void forward_kernel(float *y, const float *x,
     int n, m, h0, w0, h_base, w_base, h, w;
 
     int W_grid = ceil(1.0 * W_out / TILE_WIDTH);
+    int H_grid = ceil(1.0 * H_out / TILE_WIDTH);
+
+    int X_tile_width = TILE_WIDTH + K - 1;
+    int mask_r = floor(K * 1.0 / 2);
+
+    extern __shared__ float shmem[];
+    float* X_shared = &shmem[0];
+    float* W_shared = &shmem[X_tile_width * X_tile_width];
 
     n = blockIdx.x;
     m = blockIdx.y;
     h0 = threadIdx.y;
     w0 = threadIdx.x;
-    h_base = (blockIdx.z / W_grid) * TILE_WIDTH;
+    h_base = floor(1.0 * blockIdx.z / W_grid) * TILE_WIDTH;
     w_base = (blockIdx.z % W_grid) * TILE_WIDTH;
     h = h_base + h0;
     w = w_base + w0;
 
     float acc = 0;
-    float cur_x;
-    float cur_k;
 
-    if((n < B) && (m < M) && (h < H_out) && (w < W_out)) {
-       for(int c = 0; c < C; c++) {
-           for(int p = 0; p < K; p++) {
-               for(int q = 0; q < K; q++) {
-                   cur_x = x[(n) * (C * H * W) + (c) * (H * W) + (h + p) * (W) + (w + q)];
-                   cur_k = k[(m) * (C * K * K) + (c) * (K * K) + (p) * (K) + q];
-                   acc += cur_x * cur_k;
-               }
-           }
-       }
-   y[(n) * (M * H_out * W_out) + (m) * (H_out * W_out) + (h) * (W_out) + w] = acc;
-   }
+    for (int c = 0; c < C; ++c) {
+
+        // load kernel weights into shared memroy
+        if ((h0 < K) && (w0 < K)) {
+            // W_shared[h0, w0], k4d(m, c, h0, w0);
+            W_shared[h0 * K + w0] = k[m * (C * K * K) + c * (K * K) + h0 * (K) + w0];
+        }
+        __syncthreads();
+
+        // load input elements into shared memory
+        for (int i = h; i < h_base + X_tile_width; i += TILE_WIDTH) {
+            for (int j = w; j < w_base + X_tile_width; j += TILE_WIDTH) {
+                // X_shared[i - h_base, j - w_base], x4d(n, c, i, j)
+                if ((n < B) && (m < M) && (i < H) && (j < W)) {
+                    X_shared[(i - h_base) * X_tile_width + j - w_base] = x[n * (C * H * W) + c * (H * W) + i * (W) + j];
+                } else {
+                    X_shared[(i - h_base) * X_tile_width + j - w_base] = 0.0f;
+                }
+            }
+        }
+        __syncthreads();
+
+        // convolution
+        for (int p = 0; p < K; ++p) {
+            for (int q = 0; q < K; ++q) {
+                if ((h + p) < X_tile_width && (w + q) < X_tile_width) {
+                    // X_shared[h + p, w + q], W_shared[p, q]
+                    acc = acc + X_shared[(h + p) * X_tile_width + (w + q)] * W_shared[p * K + q];
+                }
+            }
+        }
+        __syncthreads();
+    }
+
+    // y4d[n, m, h, w]
+    if ((n < B) && (m < M) && (h < H_out) && (w < W_out)) {
+        y[n * (M * H_out * W_out) + m * (H_out * W_out) + h * (W_out) + w] = acc;
+    }
 
 // #undef y4d
 // #undef x4d
@@ -103,16 +135,18 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y,
     // dim3 blockDim(0);
     int W_out = W - K + 1;
     int H_out = H - K + 1;
-    int W_grid = ceil(1.0 * W_out / TILE_WIDTH);
-    int H_grid = ceil(1.0 * H_out / TILE_WIDTH);
+    int W_grid = ceil(W_out / TILE_WIDTH);
+    int H_grid = ceil(H_out / TILE_WIDTH);
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
     dim3 gridDim(B, M, W_grid * H_grid);
 
     // Call the kernel
     // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_, x.dptr_, w.dptr_,
     //                                             B, M, C, H, W, K);
-    forward_kernel<<<gridDim, blockDim>>>(y.dptr_, x.dptr_, w.dptr_,
-                                          B, M, C, H, W, K);
+    int X_tile_width = TILE_WIDTH + K - 1;
+    int shared_size = (X_tile_width * X_tile_width + K * K) * sizeof(float);
+    forward_kernel<<<gridDim, blockDim, shared_size>>>(y.dptr_, x.dptr_, w.dptr_,
+                                                       B, M, C, H, W, K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
